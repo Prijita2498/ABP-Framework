c5cc6ba3ece7eb750511de5efee4697253c18219
prijithaezhava <prijitha.unnikrishnan@maez.com.au>
2023-11-23 12:36:24 +0530

    Acme-BookStore project created, migrations and DbContext added

---
+++ Acme.BookStore/src/Acme.BookStore.Domain.Shared/Books/BookType.cs
@@ -0,0 +1,15 @@
+﻿namespace Acme.BookStore.Books
+{
+    public enum BookType
+    {
+        Undefined,
+        Adventure,
+        Biography,
+        Fantasy,
+        Horror,
+        Science,
+        Dystopia,
+        ScienceFiction,
+        Poetry
+    }
+}
